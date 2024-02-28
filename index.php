ab8e07d6b79b4808d087d18c0a047a5bf954b5e8
Nadarajan GS <nadarajangs31@gmail.com>
2024-02-28 06:31:57 +0000

    updated hire me section

--- index.php
+++ index.php
@@ -1,3 +1,55 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';  // Use the correct path if needed
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "nadarajangs31@gmail.com";
+    $subject = "New Contact Form Submission";
+
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message";
+
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.example.com';  // Your SMTP server
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'your_username';
+        $mail->Password   = 'your_password';
+        $mail->SMTPSecure = 'tls';  // Use 'ssl' for SSL encryption, 'tls' for TLS encryption
+        $mail->Port       = 587;    // Adjust the port accordingly
+
+        //Recipients
+        $mail->setFrom($email, $name);
+        $mail->addAddress($to);
+
+        //Content
+        $mail->isHTML(false);
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+
+        $mail->send();
+
+        // Redirect after sending the email (you can customize the URL)
+        header("Location: thank-you.html");
+        exit();
+    } catch (Exception $e) {
+        // Display an error message
+        echo "Failed to send email. Error: {$mail->ErrorInfo}";
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -61,7 +113,7 @@
                         <a class="link" href="#about">About</a>
                     </li>
                     <li class="item">
-                        <a class="link" href="#portfolio">Portfolio</a>
+                        <a class="link" href="#portfolio">Projects</a>
                     </li>
                     <!-- <li class="item">
                     <a class="link" href="#testmonial">Testmonial</a>
@@ -177,6 +229,34 @@
         </div>
     </section>
     <!-- end of service section -->
+
+      <!-- section -->
+      <section class="section-sm bg-primary">
+        <!-- container -->
+        <div class="container text-center text-sm-left">
+            <!-- row -->
+            <div class="row align-items-center">
+                <div class="col-sm offset-md-1 mb-4 mb-md-0">
+                    <h6 class="title text-light">Want to work with me?</h6>
+                    <p class="m-0 text-light">Always feel Free to Contact & Hire me</p>
+                </div>
+                <div class="col-sm offset-sm-2 offset-md-3">
+                    <!-- <a href="https://mail.google.com/mail/?view=cm&fs=1&to=nadarajangs31@gmail.com" target="_blank" class="btn btn-lg my-font btn-light rounded">Hire Me</a>
+                     -->
+                    <a
+                        href="https://mail.google.com/mail/?view=cm&fs=1&to=nadarajangs31@gmail.com"
+                        target="_blank"
+                        class="btn btn-lg my-font btn-light rounded"
+                        onclick="showSuccessModal()"
+                    >Hire Me</a>
+                </div>
+            </div>
+            <!-- end of row -->
+        </div>
+        <!-- end of container -->
+    </section>
+    <!-- end of section -->
+
     <!-- portfolio section -->
     <section class="section" id="portfolio">
         <div class="container text-center">
@@ -238,74 +318,8 @@
         <!-- end of container -->
     </section>
     <!-- end of portfolio section -->
-    <!-- section -->
-    <section class="section-sm bg-primary">
-        <!-- container -->
-        <div class="container text-center text-sm-left">
-            <!-- row -->
-            <div class="row align-items-center">
-                <div class="col-sm offset-md-1 mb-4 mb-md-0">
-                    <h6 class="title text-light">Want to work with me?</h6>
-                    <p class="m-0 text-light">Always feel Free to Contact & Hire me</p>
-                </div>
-                <div class="col-sm offset-sm-2 offset-md-3">
-                    <!-- <a href="https://mail.google.com/mail/?view=cm&fs=1&to=nadarajangs31@gmail.com" target="_blank" class="btn btn-lg my-font btn-light rounded">Hire Me</a>
-                     -->
-                    <a
-                        href="https://mail.google.com/mail/?view=cm&fs=1&to=nadarajangs31@gmail.com"
-                        target="_blank"
-                        class="btn btn-lg my-font btn-light rounded"
-                        onclick="showSuccessModal()"
-                    >Hire Me</a>
-                </div>
-            </div>
-            <!-- end of row -->
-        </div>
-        <!-- end of container -->
-    </section>
-    <!-- end of section -->
-    <!-- testimonial section -->
-    <section class="section" id="testmonial">
-        <div class="container text-center">
-            <p class="section-subtitle">What Think Client About Me ?</p>
-            <h6 class="section-title mb-6">Testmonial</h6>
-            <!-- row -->
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="testimonial-card">
-                        <div class="testimonial-card-img-holder">
-                            <img src="assets/imgs/avatar2.jpg" class="testimonial-card-img" alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page">
-                        </div>
-                        <div class="testimonial-card-body">
-                            <p class="testimonial-card-subtitle">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing
-                                elit. Eaque doloribus autem aperiam earum nostrum omnis blanditiis corporis perspiciatis
-                                adipisci nihil.
-                            </p>
-                            <h6 class="testimonial-card-title">Emily Reb</h6>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="testimonial-card">
-                        <div class="testimonial-card-img-holder">
-                            <img src="assets/imgs/avatar3.jpg" class="testimonial-card-img" alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page">
-                        </div>
-                        <div class="testimonial-card-body">
-                            <p class="testimonial-card-subtitle">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing
-                                elit. Eaque doloribus autem aperiam earum nostrum omnis blanditiis corporis perspiciatis
-                                adipisci nihil.
-                            </p>
-                            <h6 class="testimonial-card-title">Emily Reb</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- end of container -->
-    </section>
-    <!-- end of testimonial section -->
+  
+
     <!-- blog section -->
     <section class="section" id="blog">
         <!-- container -->
@@ -348,7 +362,7 @@
             <p class="section-subtitle">How can you communicate?</p>
             <h6 class="section-title mb-5">Contact Me</h6>
             <!-- contact form -->
-            <form action="" class="contact-form col-md-10 col-lg-8 m-auto">
+            <form action="" method="post" class="contact-form col-md-10 col-lg-8 m-auto">
                 <div class="form-row">
                     <div class="form-group col-sm-6">
                         <input
@@ -393,21 +407,25 @@
                 Copyright
                 <script>document.write(new Date().getFullYear())</script>
                 &copy;
-                <a href="http://www.devcrud.com">DevCRUD</a>
-                Distribution
-                <a href="https://themewagon.com">ThemeWagon</a>
+                nadarajangs31@gmail.com
             </p>
-            <div class="social-links text-right m-auto ml-sm-auto">
-                <a href="javascript:void(0)" class="link">
-                    <i class="ti-facebook"></i>
+            <div class="social-links text-right m-auto ml-sm-auto ">
+                <a href="https://www.instagram.com/nattu_g_s/" class="link" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" top="-4" fill="currentColor" class="bi bi-instagram mb-1" viewBox="0 0 16 16">
+  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+</svg>
                 </a>
-                <a href="javascript:void(0)" class="link">
-                    <i class="ti-twitter-alt"></i>
+                <a href="https://www.linkedin.com/in/nadarajangs/" target="_blank" class="link">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin mb-1" viewBox="0 0 16 16">
+  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+</svg>
                 </a>
-                <a href="javascript:void(0)" class="link">
-                    <i class="ti-google"></i>
+                <a href="https://medium.com/@nadarajangs31" class="link" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-medium mb-1" viewBox="0 0 16 16">
+  <path d="M9.025 8c0 2.485-2.02 4.5-4.513 4.5A4.506 4.506 0 0 1 0 8c0-2.486 2.02-4.5 4.512-4.5A4.506 4.506 0 0 1 9.025 8m4.95 0c0 2.34-1.01 4.236-2.256 4.236S9.463 10.339 9.463 8c0-2.34 1.01-4.236 2.256-4.236S13.975 5.661 13.975 8M16 8c0 2.096-.355 3.795-.794 3.795-.438 0-.793-1.7-.793-3.795 0-2.096.355-3.795.794-3.795.438 0 .793 1.699.793 3.795"/>
+</svg>
                 </a>
-                <a href="javascript:void(0)" class="link">
+                <!-- <a href="javascript:void(0)" class="link">
                     <i class="ti-pinterest-alt"></i>
                 </a>
                 <a href="javascript:void(0)" class="link">
@@ -415,7 +433,7 @@
                 </a>
                 <a href="javascript:void(0)" class="link">
                     <i class="ti-rss"></i>
-                </a>
+                </a> -->
             </div>
         </footer>
     </div>
